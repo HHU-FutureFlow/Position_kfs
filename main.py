63ea65748d595fcb4bfada364075d0439a02a6a5
CiruelasWenruan <121721657+CiruelasWenruan@users.noreply.github.com>
2026-01-18 21:20:31 +0800

    1.18

--- main.py
+++ main.py
@@ -4,13 +4,31 @@
 # 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
 
 #开机，测出给定像素的深度信息，将相机坐标系下三维坐标进行转换变成基坐标系下三维坐标
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+from concurrent.futures import ThreadPoolExecutor
+import Function as fc
+from ultralytics import YOLO
 
 
-# 按装订区域中的绿色按钮以运行脚本。
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    df, cf, fst = fc.RealsenseConfig()
+    model = YOLO(r'D:\Position\best.pt')
+    print(f"成功加载模型")
+    while True:
+        result = fc.YoloDetect(model,cf)
+        if result[0].boxes == None:
+            fc.drawFrame(result, df, [0,0], fst)
+            continue
+        boxes_xywh = result[0].boxes.xywh.cpu().numpy()
+        if len(boxes_xywh) == 0:
+            fc.drawFrame(result, df,[0,0], fst)
+            continue
+        Class_ids, Target_x, Target_y, Target_z = fc.positiondetect(df,result)
+        target_position = [Target_x,Target_y,Target_z]
+        fc.drawFrame(result, df, target_position, fst)
+
+
+
+
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
